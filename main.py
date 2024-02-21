fix train weight calculation

main.py
@@ -319,7 +319,7 @@ for dataset in datasets:
                                     #loss *= y_weight
                                     return pred, loss
                             
-                            try_model = TrialModel(model, out_loss, out_activ, calc_class_weight(y_train, not clw))
+                            try_model = TrialModel(model, out_loss, out_activ, calc_class_weight(y_train) if not clw else None)
 
                             def summarize_loss(loss):
                                 return torch.sum(loss).item()

utils.py
@@ -180,21 +180,14 @@ def transform_y(y, nr_classes, dataset):
     return ybinary
 
 
-def calc_class_weight(_y_train, no_weight):
+def calc_class_weight(_y_train):
 
-    class_weight = np.zeros(_y_train.shape[1])
-    def calc_cl_w():
-        print(_y_train.shape)
-        cls_freq = _y_train.mean(axis=0) 
-        print(cls_freq)
-        _w = np.max(cls_freq)/cls_freq
-        print(_w)
-        return _w/np.sum(_w)*_y_train.shape[1]
-    
-    for i, w in zip(range(_y_train.shape[1]), calc_cl_w()):
-        class_weight[i] = w if not no_weight else 1
-
-    return class_weight
+    print(_y_train.shape)
+    cls_freq = _y_train.mean(axis=0) 
+    print(cls_freq)
+    _w = np.max(cls_freq)/cls_freq
+    print(_w)
+    return _w/np.sum(_w)*_y_train.shape[1]
 
 
 # Model and dataset evaluation
@@ -221,7 +214,9 @@ def evaluate_model(_model, _X_train, _y_train, _X_test, _y_test, _epochs=20, pat
     reduce_lr = ReduceLROnPlateau(monitor='loss', factor=lr_magnif_on_plateau, patience=10,
                                   min_lr=0.0000001, verbose=1)
 
-    class_weight = calc_class_weight(_y_train, no_weight)
+    class_weight = {}
+    for i, w in zip(range(_y_train.shape[1]), calc_class_weight(_y_train)):
+        class_weight[i] = w if not no_weight else 1
 
     print(len(class_weight))
     for key in class_weight: